9f4df7f086806352d99451786c340dda97114791
rahulmane08 <rahul.mane08@gmail.com>
2017-11-10 00:53:20 +0530

    working version

---
+++ incdocs-server/src/main/java/com/incdocs/sample/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.incdocs.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@RestController
+@EnableWebMvc
+@EnableAutoConfiguration
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+	public String hello() {
+		return "hello world!";
+	}
+}
